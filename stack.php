ede81dac3de3c68996a453bbe0d936dc94844f71
rgbenergy <sebastian@sgundersen.com>
2017-06-23 12:37:29 +0200

    STACK is now an optional dependency for LaTeX querying

--- stack.php
+++ stack.php
@@ -2,14 +2,27 @@
 
 define('AJAX_SCRIPT', true);
 
-require_once(__DIR__ . '/../../config.php');
-require_once(__DIR__ . '/../../question/type/stack/stack/utils.class.php');
-require_once(__DIR__ . '/../../question/type/stack/stack/mathsoutput/mathsoutput.class.php');
-require_once(__DIR__ . '/../../question/engine/lib.php');
+require_once( __DIR__ . '/../../config.php');
 
 $input = required_param('input', PARAM_RAW);
 $input_name = required_param('name', PARAM_ALPHANUMEXT);
 $activequiz_attempt_id = required_param('id', PARAM_INT);
+
+$path_stack_utils = __DIR__ . '/../../question/type/stack/stack/utils.class.php';
+$path_stack_mathsoutput = __DIR__ . '/../../question/type/stack/stack/mathsoutput/mathsoutput.class.php';
+
+if (!file_exists($path_stack_utils) || !file_exists($path_stack_mathsoutput)) {
+    echo json_encode([
+        'status' => 'stack not found',
+        'latex' => $input,
+        'original' => $input
+    ]);
+    exit;
+}
+
+require_once(__DIR__ . '/../../question/engine/lib.php');
+require_once($path_stack_utils);
+require_once($path_stack_mathsoutput);
 
 $PAGE->set_context(context_system::instance());
 
