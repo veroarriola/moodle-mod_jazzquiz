9218da075a612383f97748ce4382c2cf8873f895
Sebastian <sebastian@sgundersen.com>
2018-06-26 13:41:32 +0200

    Fix some css

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018062601; // The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2018062606; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2016052304; // Moodle 3.1 (or above)
 $plugin->cron = 0; // Period in seconds for cron to run.
 $plugin->component = 'mod_jazzquiz';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.7.0 (Build: 2018062601)';
+$plugin->release = '0.7.0 (Build: 2018062606)';
