9ae65cc59772e34e644704d5c59905e4a241ad91
rgbenergy <sebastian@sgundersen.com>
2019-06-19 12:48:59 +0200

    Fix the js build files Forgot to transpile

--- amd/build.sh
+++ amd/build.sh
@@ -1,10 +1,11 @@
-echo 'Copying...';
+#!/bin/bash
+echo 'Copying...'
 cp -R src orig
-echo 'Running Babel...';
+echo 'Running Babel...'
 babel orig -d src
-echo 'Running Grunt...';
+echo 'Running Grunt...'
 grunt amd --force
-echo 'Cleaning up...';
+echo 'Cleaning up...'
 rm -R src
 mv orig src
-echo 'Done.';
+echo 'Done.'
