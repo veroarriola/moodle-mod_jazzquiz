facd98f68435c4e7e41510875577441daacd74ae
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2021-02-05 10:24:06 +0100

    Updated version.php

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019073000; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2021020400; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2018051700; // Moodle 3.5 (or above).
 $plugin->cron = 0; // Period in seconds for cron to run.
 $plugin->component = 'mod_jazzquiz';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0 (Build: 2019073000)';
+$plugin->release = '1.0.1 (Build: 2021020400)';
